2d68b555bd7d96727fd4c56d5c3a21313a9056f2
nithu14 <nithanthrai@gmail.com>
2021-05-24 13:09:15 +0530

    code optimization and added unit testing id length

--- src/main/java/StudentTest.java
+++ src/main/java/StudentTest.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Studenttest {
+	static LinkedList<String> NA = new LinkedList<String>();
+	static LinkedHashMap<String, String> Veg = new LinkedHashMap<String,String>();
+	static LinkedHashMap<String, String> NVeg = new LinkedHashMap<String,String>();
 	
 	public static int checkno(String no) {
 		// to check for valid int entered
@@ -24,34 +25,94 @@ public class Studenttest {
 		if(size<=0) {
 			System.out.println("Please enter valid no");
 			return size;
-		}
-		
+		}	
 		return size;
+	}
+	
+	public static void addstudent(Scanner sc,int size) {
+		int j=0;
+		String req;
+		ArrayList<String> al = new ArrayList<String>();
+		new ArrayList<String>();
+		String[] sp = null;
 		
+		while(j<size) {
+			
+			req = sc.nextLine();
+			
+			if(req.contains("fin")) {
+				break;
+			}else if(j==size) {
+				System.out.println("Size exceeded");
+				break;
+			}
+			
+			// check for id length
+			String[] split = req.split(" ");
+			
+			if(split[1].length()>4) {
+				System.out.println("not a valid id, please enter again");
+			}
+			else if(al.contains(split[1])) {
+				System.out.println("duplicate Student, please enter again");
+			}
+			else
+				al.add(req);
+				
+			j++;
+			
+		}	
 		
-		
+		// Separate students based on their food preference 
+				for(int i=0;i<al.size();i++) {
+					if(al.get(i).contains("NV")) {
+						sp = al.get(i).split("\\s");
+						NVeg.put(sp[1], sp[2]);
+			
+					}else if(al.get(i).contains("V")) {
+						sp = al.get(i).split("\\s");
+						Veg.put(sp[1], sp[2]);
+			
+					}else {
+						sp = al.get(i).split("\\s");
+						NA.add(sp[1]);
+					}
+				}
 	}
 
-	@SuppressWarnings("resource")
+	public static Object[] Separator(LinkedHashMap<String, String> hm) {
+//		ArrayList<String>obj =new ArrayList<String>();
+		Object[] obj=new Object[2];
+		ArrayList<String> AV=new ArrayList<String>();
+		ArrayList<String> BV=new ArrayList<String>();
+		
+		Set<String> key = hm.keySet();
+		Iterator<String> it1 = key.iterator();
+		while(it1.hasNext()) {
+			String stu = it1.next();
+			if(hm.get(stu).equalsIgnoreCase("A")) {	
+				AV.add(stu);
+			}
+			else if(hm.get(stu).equalsIgnoreCase("B"))
+				BV.add(stu);
+			else
+				NA.add(stu);
+			}
+		
+		obj[0]=AV;
+		obj[1]=BV;
+		
+		return obj;		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int size=0;
 		Scanner sc = new Scanner(System.in);
-		ArrayList<String> al = new ArrayList<String>();
-		ArrayList<String> AV=new ArrayList<String>();
-		ArrayList<String> ANV=new ArrayList<String>();
-		ArrayList<String> BV=new ArrayList<String>();
-		ArrayList<String> BNV=new ArrayList<String>();
-		LinkedList<String> NA = new LinkedList<String>();
-		
-		LinkedHashMap<String, String> Veg = new LinkedHashMap<String,String>();
-		LinkedHashMap<String, String> NVeg = new LinkedHashMap<String,String>();
-		
-
-	// check untill valid int is entered	
+		Object[] sep=new Object[2];
+		// check untill valid int is entered	
 		boolean flag=true;
-		while(flag) {
-			
+		while(flag) {	
 			String count = sc.nextLine();
 			String[] cnt = count.split("\\s");
 			size=checkno(cnt[1]);
@@ -62,81 +123,20 @@ public class Studenttest {
 			}
 		}
 		
-				
-		int j=0;
-		String req;
-		// add student record untill size or eof is reached 
-		while(j<size) {
+	// add students for size given			
+		addstudent(sc,size);
 		
-			req = sc.nextLine();
-			
-			if(req.contains("fin")) {
-				break;
-			}else if(j==size) {
-				System.out.println("Size exceeded");
-				break;
-			}
-				al.add(req);
-				j++;
-			
-		}
+	// Separate students based on class and print
 		
-		String[] sp = null;
-		
-		// Separate students based on their food preference 
-		for(int i=0;i<al.size();i++) {
-			if(al.get(i).contains("NV")) {
-				sp = al.get(i).split("\\s");
-				NVeg.put(sp[1], sp[2]);
-	
-			}else if(al.get(i).contains("V")) {
-				sp = al.get(i).split("\\s");
-				Veg.put(sp[1], sp[2]);
-	
-			}else {
-				sp = al.get(i).split("\\s");
-				NA.add(sp[1]);
-			}
-		}
-				
-		// Separate students based on class
-		
-		Set<String> Vkey = Veg.keySet();
-		Iterator<String> it1 = Vkey.iterator();
-		while(it1.hasNext()) {
-			String stu = it1.next();
-			if(Veg.get(stu).equalsIgnoreCase("A")) {	
-				AV.add(stu);
-			}
-			else if(Veg.get(stu).equalsIgnoreCase("B"))
-				BV.add(stu);
-			else
-				NA.add(stu);
-			}
-		
-		
-		Set<String> NKey = NVeg.keySet();
-		Iterator<String> it2 = NKey.iterator();
-		while(it2.hasNext()) {
-			String stu = it2.next();
-			if(NVeg.get(stu).equalsIgnoreCase("A")) {
-				
-				ANV.add(stu);
-				
-			}
-			else if(NVeg.get(stu).equalsIgnoreCase("B"))
-				BNV.add(stu);
-			else
-				NA.add(stu);
-			}
-		
-		//print final output
-		System.out.println();
-		System.out.println("AV :"+AV);
-		System.out.println("BV :"+BV);
-		System.out.println("ANV :"+ANV);
-		System.out.println("BNV :"+BNV);
+		sep=Separator(Veg);	
+		System.out.println("AV :"+sep[0].toString());
+		System.out.println("BV :"+sep[1].toString());
+
+		sep=Separator(NVeg);	
+		System.out.println("ANV :"+sep[0].toString());
+		System.out.println("BNV :"+sep[1].toString());
 		System.out.println("NA :"+NA);
+		
 	}
 
 }
